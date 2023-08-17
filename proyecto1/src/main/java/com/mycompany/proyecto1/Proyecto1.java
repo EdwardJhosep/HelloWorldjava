6f1389b5e05f09ff08b8d76d4f9c0d036b3b4931
EdwardJhosep <1454668@senati.pe>
2023-08-17 10:10:18 -0500

    creamos un menu simple para elegir la suma y resta

--- proyecto1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
+++ proyecto1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
@@ -13,13 +13,19 @@ import java.util.Scanner;
 public class Proyecto1 {
 
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
 System.out.println("Hello World!");
         
-
+        int p = 0;
         int a = 0;
         int b = 0;
+        int rs =0;
+        //agregamos un menu en donde vamos a elegir la suma o resta 
+        System.err.println("que operacion deseas realizar");
+        System.err.println("1...suma");
+        System.err.println("2...resta");
+        p = reader.nextInt();
         
-
         
     }
 }
