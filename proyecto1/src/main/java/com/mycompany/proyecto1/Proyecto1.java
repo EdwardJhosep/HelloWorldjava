14fce22e75c6c58b619a1f70e660f02c608b2b84
EdwardJhosep <1454668@senati.pe>
2023-08-17 09:51:04 -0500

    creacion de variables

--- proyecto1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
+++ proyecto1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
@@ -12,5 +12,14 @@ public class Proyecto1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        int opcion ;
+        int a;
+        int b;
+        int rs;
+        
+        
+        
+        
     }
 }
